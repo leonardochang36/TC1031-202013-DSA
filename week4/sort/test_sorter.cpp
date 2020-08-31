13be4825d0e9e9a56a88a57b51d7c1e51488da4a
AAA <leonardochang36@gmail.com>
2020-08-31 17:11:00 -0400

    add timing to test_sorter

--- week4/sort/test_sorter.cpp
+++ week4/sort/test_sorter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <stdlib.h>
+#include <stdlib.h>  // for std::rand
+#include <algorithm> // for std::sort
 #include "sorter.h"
 #include "mytimer.h"
 
@@ -20,23 +21,35 @@ void print_vector(vector<T> arr, int N)
 int main()
 {
     // generate a random vector array of float
-    vector<int> arr_f;
-    int count = 10;
+    int count = 10000;
+    cout << "ARRAY SIZE: " << count << endl;
+    vector<float> arr_f;
     for (size_t i = 0; i < count; i++)
     {
-        arr_f.push_back((int)rand() % 256);
-        // arr_f.push_back((double)rand() / RAND_MAX);
+        arr_f.push_back((float)rand() / RAND_MAX);
     }
-
-    cout << "Unsorted:" << endl;
-    print_vector(arr_f, 10);
-
+    vector<float> arr_s(arr_f);
+    
     // create instance of SelectionSort
-    SelectionSort<int> selsort;
+    DECLARE_TIMING(t);
+    START_TIMING(t);
+    SelectionSort<float> selsort;
     selsort.sort(arr_f);
+    STOP_TIMING(t);
+    SHOW_TIMING(t, "SelectionSort");
 
-    cout << endl << "Sorted:" << endl;
-    print_vector(arr_f, 10);
+    // use std::sort
+    DECLARE_TIMING(t1);
+    START_TIMING(t1);
+    std::sort(arr_s.begin(), arr_s.end());
+    STOP_TIMING(t1);
+    SHOW_TIMING(t1, "std::sort");
 
+    // check if equal
+    if (arr_f == arr_s)
+        cout << "CONGRATS, your implementation is correct!" << endl;
+    else
+        cout << "Ops, sorry cowboy (cowgirl), it seems that something is still missing in your implementation" << endl;
+    
     return 0;
 }
