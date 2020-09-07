2c26894a72e3151fa8cc87a0cdd7157ecf0db6b5
AAA <leonardochang36@gmail.com>
2020-09-07 16:00:02 -0400

    removing comments

--- week4/sort/test_sorter.cpp
+++ week4/sort/test_sorter.cpp
@@ -57,32 +57,32 @@ int main()
     STOP_TIMING(t);
     SHOW_TIMING(t, "std::sort");
 
-    // // create instance of SelectionSort
-    // DECLARE_TIMING(t1);
-    // START_TIMING(t1);
-    // SelectionSort<float> selsort;
-    // selsort.sort(arr_1);
-    // STOP_TIMING(t1);
-    // compare_vectors(arr_f, arr_1, "Selection sort");
-    // SHOW_TIMING(t1, "SelectionSort:");
+    // create instance of SelectionSort
+    DECLARE_TIMING(t1);
+    START_TIMING(t1);
+    SelectionSort<float> selsort;
+    selsort.sort(arr_1);
+    STOP_TIMING(t1);
+    compare_vectors(arr_f, arr_1, "Selection sort");
+    SHOW_TIMING(t1, "SelectionSort:");
 
-    // // create instance of BubbleSort
-    // DECLARE_TIMING(t2);
-    // START_TIMING(t2);
-    // BubbleSort<float> bubsort;
-    // bubsort.sort(arr_2);
-    // STOP_TIMING(t2);
-    // compare_vectors(arr_f, arr_2, "Bubble sort:");
-    // SHOW_TIMING(t2, "BubbleSort");
+    // create instance of BubbleSort
+    DECLARE_TIMING(t2);
+    START_TIMING(t2);
+    BubbleSort<float> bubsort;
+    bubsort.sort(arr_2);
+    STOP_TIMING(t2);
+    compare_vectors(arr_f, arr_2, "Bubble sort:");
+    SHOW_TIMING(t2, "BubbleSort");
     
-    // // create instance of InsertionSort
-    // DECLARE_TIMING(t3);
-    // START_TIMING(t3);
-    // InsertionSort<float> inssort;
-    // inssort.sort(arr_3);
-    // STOP_TIMING(t3);
-    // compare_vectors(arr_f, arr_3, "InsertionSort sort:");
-    // SHOW_TIMING(t3, "InsertionSort");
+    // create instance of InsertionSort
+    DECLARE_TIMING(t3);
+    START_TIMING(t3);
+    InsertionSort<float> inssort;
+    inssort.sort(arr_3);
+    STOP_TIMING(t3);
+    compare_vectors(arr_f, arr_3, "InsertionSort sort:");
+    SHOW_TIMING(t3, "InsertionSort");
 
 
     // create instance of Quicksort
